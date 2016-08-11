422399201a44a6e661b55e5f96c430d30e6be8b9
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2016-08-10 23:59:00 -0400

    Horizontal text embedding works great. Code is a bit messy though.

--- termatrix.py
+++ termatrix.py
@@ -25,23 +25,26 @@ class DisplayCharacter(object):
         return Point(self.rendered_text.get_width(), self.rendered_text.get_height())
 
 class MatrixRain(object):
-    def __init__(self, dimensions, font_widths, font_rain, font_text, text_embeds):
-        self.max_dims = Point(max([v.x for v in font_widths.values()]), max([f.get_height() for f in [font_rain, font_text]]))
+    def __init__(self, dimensions, font_rendered_dim, font_rain, font_text, text_embeds, embed_interval_ms):
+        self.max_dims = Point(max([v.x for v in font_rendered_dim.values()]), max([f.get_height() for f in [font_rain, font_text]]))
         self.font_rain = font_rain
         self.font_text = font_text
-        self.text_embeds = text_embeds
-        self.font_widths = font_widths
         self.pixel_dimensions = dimensions
         self.surface = pygame.Surface(dimensions, pygame.HWSURFACE)
 
-        self.row_colors, self.row_render = self.generate_row(font_rain, self.max_dims.x, self.max_dims.y, self.pixel_dimensions.x, (0, 0, 0))
+        self.row_colors, self.row_render = self.generate_row(font_rain, font_text, self.max_dims.x, self.max_dims.y, self.pixel_dimensions.x, (0, 0, 0))
 
         # row_portion keeps track of how much of the current row is exposed
         self.row_portion = 0
         self.row_height = font_rain.get_height()
 
+        self.last_text_embed = pygame.time.get_ticks()
+        self.text_embed_index = -1 if len(text_embeds) == 0 else 0
+
+        self.get_text_embed = lambda: self.get_text_embed_full(text_embeds, embed_interval_ms)
+
     # if previous is an array, it will be reused!
-    def generate_row(self, font, char_width, char_height, row_width, bg_color, previous = None):
+    def generate_row(self, font_rain, font_text, char_width, char_height, row_width, bg_color, text_embed = None, previous = None):
         char_cell_height = char_height
         char_count = int(row_width / char_width)
         char_cell_width = row_width / char_count # leave as float till later
@@ -50,14 +53,35 @@ class MatrixRain(object):
 
         row_surface = pygame.Surface((row_width, char_cell_height), pygame.HWSURFACE)
 
+        text_range = range(0, 0)
+        if text_embed:
+            start = random.randrange(0, char_count - len(text_embed), 1)
+            text_range = range(start, start + len(text_embed))
+
         row_color = previous if isinstance(previous, np.ndarray) else np.zeros(char_count, dtype=[('r', 'i4'), ('g', 'i4'), ('b', 'i4')])
         for i in range(0, char_count):
             char = chr(random.randrange(0x3041, 0x308F, 0x1)) # Pick a random Hiragana character (U+3040 - U+309F)
             row_color[i] = Color(previous[i]['r'], previous[i]['g'] - 1 if previous[i]['g'] > 10 else 190, previous[i]['b']) if isinstance(previous, np.ndarray) else Color(0, random.randrange(10, 190, 1), 0)
-            char_render = font.render(char, True, row_color[i], bg_color)
+            char_render = font_rain.render(char, True, row_color[i], bg_color)
             row_surface.blit(char_render, (int(char_cell_width * i + (char_cell_width - char_render.get_width()) / 2), int((char_cell_height - char_render.get_height()) / 2)))
 
-        return (row_color, row_surface)
+            if i in text_range:
+                char_render = font_text.render(text_embed[i - text_range[0]], True, (0, 255, 0), bg_color)
+                row_surface.blit(char_render, (int(char_cell_width * i + (char_cell_width - char_render.get_width()) / 2), int((char_cell_height - char_render.get_height()) / 2)))
+
+        return (row_color, row_surface.convert())
+
+    def get_text_embed_full(self, text_embeds, embed_interval):
+        if self.text_embed_index < 0 or self.text_embed_index >= len(text_embeds):
+            return None
+
+        now = pygame.time.get_ticks()
+        if now - self.last_text_embed < embed_interval:
+            return None
+
+        self.text_embed_index += 1
+        self.last_text_embed = now
+        return text_embeds[self.text_embed_index - 1]
 
     # Scroll the whole surface by one row, render current row at it's new location. Generate the next row if needed.
     def advance(self):
@@ -68,7 +92,7 @@ class MatrixRain(object):
         if (self.row_portion > self.row_height):
             self.surface.blit(self.row_render, (0, self.row_portion - self.row_height))
             self.row_portion = abs(self.row_height - self.row_portion)
-            self.row_colors, self.row_render = self.generate_row(self.font_rain, self.max_dims.x, self.max_dims.y, self.pixel_dimensions.x, (0, 0, 0), self.row_colors)
+            self.row_colors, self.row_render = self.generate_row(self.font_rain, self.font_text, self.max_dims.x, self.max_dims.y, self.pixel_dimensions.x, (0, 0, 0), self.get_text_embed(), self.row_colors)
 
         self.surface.blit(self.row_render, (0, self.row_portion - self.row_height))
 
@@ -80,7 +104,7 @@ def main_pygame():
 
     pygame.init()
 
-    display_dim = Point(1920, 1080)
+    display_dim = Point(1280, 720)
     pygame.display.set_caption('termatrix')
     print(pygame.display.get_driver())
     print(pygame.display.Info())
@@ -92,16 +116,16 @@ def main_pygame():
     font_japanese = pygame.font.SysFont([f for f in fonts if 'mikachan' in f][0], 10)
     font_english = pygame.font.SysFont([f for f in fonts if 'inconsolata' in f][0], 10)
     font_dims = find_dimensions([(font_japanese, chr(0x3041)), (font_english, 'D')])
-    matrix_rain = MatrixRain(display_dim, font_dims, font_japanese, font_english, sys.argv[1:])
+    matrix_rain = MatrixRain(display_dim, font_dims, font_japanese, font_english, sys.argv[1:], 5000)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 return
         matrix_rain.advance()
-        screen.blit(matrix_rain.surface, (0, 0))
+        screen.blit(matrix_rain.surface.convert(), (0, 0))
         pygame.display.flip()
-        print(clock.get_fps())
+        #print(clock.get_fps())
         clock.tick(60)
 
 if __name__ == '__main__':
